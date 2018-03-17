2eabcbca94628d66630bc68665ec5b2f2cb3ccc7
5h4hid5 <shahid.bracu@gmail.com>
2018-03-17 21:37:52 +0600

    Added grid system

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SearchForm from './components/molecules/SearchForm';
+import {Grid} from 'griz';
+import Home from './components/pages/Home';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-    <SearchForm />
-  </div>
+  <Grid>
+    <Home/>
+  </Grid>
 );
 
 export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {GridCol} from 'griz';
+
+const Home = () => (
+  <GridCol>
+    <h1>Hello</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, illum rerum assumenda in fugiat eum animi, repellat exercitationem harum ab adipisci corrupti reiciendis hic nemo qui quibusdam molestiae? Iusto, in.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae cupiditate hic provident? Nihil, porro nobis magnam ad eius. Blanditiis odit quod iure alias modi vero, ea dolorum id excepturi necessitatibus!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab deleniti, nulla possimus doloribus inventore! Esse asperiores, aut amet quos perferendis sint ex illum quas dolorem molestias tenetur nostrum illo atque?</p>
+  </GridCol>
+);
+
+export default Home;
